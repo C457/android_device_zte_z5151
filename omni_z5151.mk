8366d0381238c5a244dd07bf082ef20f7acfa1a6
Thomas Johnson <thojohns@nmu.edu>
2019-07-31 06:16:18 -0400

    Added device-specific files

---
+++ omni_z5151.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, vendor/omni/config/common.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    charger
+
+# Encryption
+PRODUCT_PACKAGES += \
+    libcryptfs_hw
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := z5151
+PRODUCT_NAME := omni_z5151
+PRODUCT_BRAND := ZTE
+PRODUCT_MODEL := Z5151V
+PRODUCT_MANUFACTURER := ZTE
