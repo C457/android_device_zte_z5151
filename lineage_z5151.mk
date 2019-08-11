c38b5feb36d6966474384ca60429b3ff5e2efb0e
C457 <android.c357@gmail.com>
2019-08-11 16:07:38 -0500

    crinum: Convert to Lineage build

---
+++ lineage_z5151.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/zte/z5151/device.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_BRAND := ZTE
+PRODUCT_DEVICE := z5151
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_MODEL := Z5151V
+PRODUCT_NAME := lineage_z5151
